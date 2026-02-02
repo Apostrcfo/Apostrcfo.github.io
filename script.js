457c5ae0cabcd6d3fc25d227595c55c2e2935ac3
Apostrcfo <protonsctaccount@proton.me>
2026-02-02 16:56:16 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById("button");
+const erro = document.getElementById("erro404");
+
+botao.addEventListener("click", () => {
+  erro.style.display = "block";
+  
+  if( erro.style.display === "block") {
+console.error("Error 404, causando por:", erro404)
+}
+});
+
